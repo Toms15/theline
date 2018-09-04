e5769deee2ca8a43d220d8b5c2a9605b7a4b674a
Toms15 <info@tommasocostantini.it>
2018-09-04 19:17:06 +0200

    Remove related video

--- wp-content/themes/the-line/section/page/content-video.php
+++ wp-content/themes/the-line/section/page/content-video.php
@@ -11,5 +11,35 @@ $video = get_sub_field('file');
 ?>
 
 <div class="responsive-embed panorama">
-  <?php echo $video; ?>
+	<?php
+
+// get iframe HTML
+	$iframe = get_sub_field('file');
+
+
+// use preg_match to find iframe src
+	preg_match('/src="(.+?)"/', $iframe, $matches);
+	$src = $matches[1];
+
+
+// add extra params to iframe src
+	$params = array(
+		'rel'    => 0
+	);
+
+	$new_src = add_query_arg($params, $src);
+
+	$iframe = str_replace($src, $new_src, $iframe);
+
+
+// add extra attributes to iframe html
+	$attributes = 'frameborder="0"';
+
+	$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+
+// echo $iframe
+	echo $iframe;
+
+	?>
 </div>
