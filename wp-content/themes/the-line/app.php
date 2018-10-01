3d97814f2a728590fe68d12ed85a2ed5447d637c
Toms15 <info@tommasocostantini.it>
2018-10-01 16:05:14 +0200

    App page Template

---
+++ wp-content/themes/the-line/app.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template name: App
+ *
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package The_Line
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main app__page">
+
+		<?php
+			get_template_part( 'template-parts/content', 'page' );
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
